f7c43f63efbec15a12bcf2edb0a03391de751489
Lassi Kortela <lassi@lassi.io>
2021-01-22 02:57:23 +0200

    Move base64 stress test to its own file
    The large-string tests take several minutes without stopping, so don't
    run them as part of the main test suite.

---
+++ tests/base64-stress.scm
@@ -0,0 +1,23 @@
+(import (scheme base) (live encoding filter base64) (live test))
+
+(test-begin "live/encoding/filter/base64")
+
+;; to avoid measuring time in test (doesn't really matter)
+(define large-string (make-string 10000001 #\a))
+(define large-encoded-string (base64-encode large-string))
+(define large-invalid-string (make-string 10000001 #\%))
+
+(test-group
+ "decoding"
+ (test-equal "decode large string"
+       large-string
+       (base64-decode large-encoded-string))
+ (test-equal "decode large string of invalid chars"
+       ""
+       (base64-decode large-invalid-string)))
+
+;; Not on a 64-bit machine! :)
+;; (test-error "encode string of length 16,000,000 signals an error"
+;;             (base64-encode (make-string 16000000)))
+
+(test-end)
--- tests/base64.scm
+++ tests/base64.scm
@@ -107,11 +107,6 @@
                                              (open-output-string)))))
    ))
 
-;; to avoid measuring time in test (doesn't really matter)
-(define large-string (make-string 10000001 #\a))
-(define large-encoded-string (base64-encode large-string))
-(define large-invalid-string (make-string 10000001 #\%))
-
 (test-group
  "decoding"
  (test-equal "decode empty string -> empty"
@@ -147,18 +142,8 @@
  (test-equal "decode binary string"
        "\xde\xad\xbe\xef\xca\xfe\xb0\x0b"
        (base64-decode "3q2+78r+sAs="))
- (test-equal "decode large string"
-       large-string
-       (base64-decode large-encoded-string))
- (test-equal "decode large string of invalid chars"
-       ""
-       (base64-decode large-invalid-string))
  (test-equal "decode lorem ipsum with linebreaks"
        lorem-ipsum
        (base64-decode (string-intersperse lorem-ipsum-base64 "\r\n"))))
-
-;; Not on a 64-bit machine! :)
-;; (test-error "encode string of length 16,000,000 signals an error"
-;;             (base64-encode (make-string 16000000)))
 
 (test-end)
