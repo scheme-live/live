22b60a181c2062c6962db77de3ed8bfe55988da6
Lassi Kortela <lassi@lassi.io>
2021-05-02 18:16:44 +0300

    Use lisp-local for Emacs indentation
    Avoids the warnings about using `eval`.
    
    https://melpa.org/#/lisp-local

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,10 @@
 ;; The 'nil' configuration applies to all modes.
-((scheme-mode . ((indent-tabs-mode . nil)
-		 (tab-width . 2)
-		 (eval . (progn
-                           ;; okvs
-			   (put 'switch 'scheme-indent-function 1)
-			   (put 'call-with-input-file 'scheme-indent-function 1)
-			   (put 'call-with-values 'scheme-indent-function 1)
-			   (put 'match 'scheme-indent-function 1))))))
+((scheme-mode
+  (indent-tabs-mode . nil)
+  (tab-width . 2)
+  (lisp-local-indent
+   ;; okvs
+   switch 1
+   call-with-input-file 1
+   call-with-values 1
+   match 1)))
