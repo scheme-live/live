929a71104f6598f53bc900d26993c0ab0ba39de3
Lassi Kortela <lassi@lassi.io>
2021-05-08 10:36:11 +0300

    Add generator test runner

---
+++ tests/generator.scm
@@ -0,0 +1,9 @@
+(import (scheme base)
+        (scheme file)
+        (scheme read)
+        (live generator)
+        (live list)
+        (live string)
+        (live test))
+
+(include "srfi-158-tests.scm")
