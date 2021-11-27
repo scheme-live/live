93cf22995a6c7b4615e29c8b9300f97588605a8c
Lassi Kortela <lassi@lassi.io>
2021-11-27 22:49:53 +0200

    Re-indent unwind-protect

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,7 @@
    c-lambda 2
    let*-pointers 1
    test-group 1
+   unwind-protect 0
    ;; okvs:
    call-with-input-file 1
    call-with-values 1
--- tests/private.scm
+++ tests/private.scm
@@ -7,28 +7,28 @@
   (test-equal '(1)
     (let ((nums '()))
       (unwind-protect
-       (lambda ()
-         (set! nums (append nums '(1)))
-         nums)
-       (lambda ()
-         (set! nums (append nums '(2)))))))
+        (lambda ()
+          (set! nums (append nums '(1)))
+          nums)
+        (lambda ()
+          (set! nums (append nums '(2)))))))
 
   (test-equal '(1 2)
     (let ((nums '()))
       (unwind-protect
-       (lambda () (set! nums (append nums '(1))))
-       (lambda () (set! nums (append nums '(2)))))
+        (lambda () (set! nums (append nums '(1))))
+        (lambda () (set! nums (append nums '(2)))))
       nums))
 
 
   (test-equal '((1) (1 2))
     (let* ((nums '())
            (a (unwind-protect
-               (lambda ()
-                 (set! nums (append nums '(1)))
-                 nums)
-               (lambda ()
-                 (set! nums (append nums '(2))))))
+                (lambda ()
+                  (set! nums (append nums '(1)))
+                  nums)
+                (lambda ()
+                  (set! nums (append nums '(2))))))
            (b nums))
       (list a b))))
 
