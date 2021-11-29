9f3b54e3fa8cad6d9884acb0d10e9ce915af0cbd
Lassi Kortela <lassi@lassi.io>
2021-11-29 23:41:11 +0200

    Add string-split procedure

---
+++ live/string/live.scm
@@ -0,0 +1,51 @@
+(define (string-last-index str)
+  (let ((n (string-length str)))
+    (if (zero? n) #f (- n 1))))
+
+(define (string-char-prefix? char str)
+  (and (not (string-null? str))
+       (char=? char (string-ref str 0))))
+
+(define (string-char-suffix? char str)
+  (let ((i (string-last-index str)))
+    (and i (char=? char (string-ref str i)))))
+
+(define (string-blank? str)
+  (string-every char-whitespace? str))
+
+(define (string->one-char-strings string)
+  (let loop ((strings '()) (n (string-length string)))
+    (if (zero? n) strings
+        (loop (cons (string-copy string (- n 1) n) strings) (- n 1)))))
+
+;; SRFI 140 (Immutable Strings) has the following:
+;;
+;; (string-split string delimiter [grammar limit start end]) -> list
+
+(define (string-split string delimiter)
+  (if (string-null? delimiter)
+      (string->one-char-strings string)
+      (reverse
+       (let loop ((parts '()) (a 0))
+         (if (> a (string-length string))
+             parts
+             (let* ((b (string-contains string delimiter a))
+                    (part (string-copy
+                           string a (or b (string-length string))))
+                    (parts (cons part parts)))
+               (if b
+                   (loop parts (+ b (string-length delimiter)))
+                   parts)))))))
+
+(define (with-input-from-string str proc)
+  (call-with-port (open-input-string str)
+                  (lambda (in)
+                    (parameterize ((current-input-port in))
+                      (proc)))))
+
+(define (with-output-to-string proc)
+  (call-with-port (open-output-string)
+                  (lambda (out)
+                    (parameterize ((current-output-port out))
+                      (proc))
+                    (get-output-string out))))
